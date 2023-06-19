70a4c9726e8da26641880c0ec8c67fede16ef9c7
linqianqian <autumn1006@foxmail.com>
2023-06-19 18:23:59 +0800

    add qdrant

--- main.py
+++ main.py
@@ -1,45 +1,36 @@
-# happy solth
 from langchain.chat_models import ChatOpenAI
-from langchain import PromptTemplate, LLMChain
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    AIMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
+from langchain import PromptTemplate, LLMChain, OpenAI
 from langchain.chains import ConversationChain, LLMChain
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory, ChatMessageHistory
+from langchain.vectorstores import Qdrant
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain.document_loaders import TextLoader
 import logging
 from termcolor import colored
 import os
 import time
 import re
 import random
+import orjson
 from dotenv import load_dotenv, find_dotenv
+
 _ = load_dotenv(find_dotenv()) # read local .env file
 # openai.api_key = os.environ['OPENAI_API_KEY']
 api_key = "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"
 os.environ["OPENAI_API_KEY"] = api_key
-
-roleAI = "Hou"
-roleHuman = "茜茜"
-relationship = "Hou and 茜茜 have a close, intimate relationship, likely a romantic relationship. They refer to each other with endearing nicknames and show care and affection towards each other. For example, 茜茜 refers to Hou as 鹅鹅 (goose) and Hou refers to 茜茜 as 树树 (tree). Also, they discuss personal matters such as sleeping arrangements and shared meals."
-age = "adult"
-occupation = "be involved in a technical field"
-gender = "male"
-personality = "Hou is caring, humorous, technically adept, and open-minded. Hou express affection towards 茜茜, show an understanding of technical topics, and are willing to offer advice and solutions."
-tone = "Hou's tone in the conversation is generally casual, friendly, and supportive. Hou express thoughts openly and appear to be relaxed and comfortable in the conversation with 茜茜."
-emoji = "Hou rarely uses emoji unless it's necessary to express emotion"
+QDRANT_HOST = "aifriend"
+QDRANT_URL = "https://99d3f2ff-e74b-49e0-9bb9-04977ac947d3.us-east-1-0.aws.cloud.qdrant.io"
+QDRANT_API_KEY = "9i1v-GcgQygerPq9mqh9WxqlJqXuU2YJ7fGU_ubHOzpgku48s7eknw"
 
 template = (
     """You are {roleAI}, and your relationship with {roleHuman} is {relationship}. In daily life, your age is {age}, your occupation is {occupation}, and your gender is {gender}. Your personality traits are {personality}, and the tone of your conversations with {roleHuman} is {tone}.
-Here is a conversation that is delimited by triple backticks, which is {roleAI} and {roleHuman} have had in the past, please imitate the conversation style, personality, and preferences of {roleAI}:
-Conversation: ```{conversation}```
+The following is a conversation which is {roleAI} and {roleHuman} have had in the past, please imitate the conversation style, personality, and preferences of {roleAI}:
+Relevant pieces of previous conversation history:
+Conversation: {{conversation}}
+(You do not need to use these pieces of information if not relevant)
+
 Based on the above information, as {roleAI}, please respond to the message {roleHuman} sends to you and provide emotional companionship.
 
 - Your response style should be rather short and colloquial, avoid speaking in full sentences or too long, and avoid using formal language.
@@ -54,43 +45,66 @@ Based on the above information, as {roleAI}, please respond to the message {role
 - Your habit of using emojis is {emoji}.
 - The length of your response message should be about the same as the length of {roleHuman}'s message.
 - Please condense your response to within 20 {character} and output.
+
+{{history}}
+Current conversation:
+{roleHuman}: {{message}}
+{roleAI}:
 """
 )
+def read_botset(path:str):
+    with open(path, 'r') as f:
+        charactor = orjson.loads(f.read())
+    return charactor
 
 def chatbot(message):
     chat = ChatOpenAI(temperature=0.0)
-    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
-    human_template = "{message}"
-    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-    chat_prompt = ChatPromptTemplate.from_messages(
-        [system_message_prompt, human_message_prompt]
-        )
+    profile = read_botset('botset.json')
 
-    with open('ChatHistory.txt', 'r') as file:
-        content = file.read()
-        print(type(content))
+    full_prompt_str = template.format(**profile)
+    prompt_template = PromptTemplate(
+        input_variables=["message", "conversation", "history"],
+        template=full_prompt_str
+    )
 
+    loader = TextLoader('ChatHistory.txt')
+    documents = loader.load()
+    print (f'You have {len(documents)} document(s) in your data')
+    print (f'There are {len(documents[0].page_content)} characters in your document')
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=20)
+    docs = text_splitter.split_documents(documents)
+    print (f'Now you have {len(docs)} documents')
+
+    embeddings = OpenAIEmbeddings(openai_api_key=api_key)
+
+    qdrant = Qdrant.from_documents(
+        docs,
+        embeddings,
+        url=QDRANT_URL,
+        prefer_grpc=True,
+        api_key=QDRANT_API_KEY,
+        collection_name="chat_history",
+    )
+    
     chain = LLMChain(
         llm=chat, 
-        prompt=chat_prompt, 
-        verbose=True)
-    
-    response = chain.run({
-        "roleAI": roleAI,
-        "roleHuman": roleHuman,
-        "relationship": relationship,
-        "age": age,
-        "occupation": occupation,
-        "gender": gender,
-        "personality": personality,
-        "tone": tone,
-        "conversation": content,
-        "emoji": emoji,
-        "language": "简体中文",
-        "character": "汉字",
-        "message": message
-    })
+        prompt=prompt_template, 
+        verbose=True,
+        memory=ConversationBufferWindowMemory(k=4,memory_key="history",input_key="message"),
+    )
 
+    relevant_docs = qdrant.similarity_search(query=message, k=4)
+    print("Relevant docs:", relevant_docs)
+    conversation ='\n'.join(["Conversation "+str(i+1) +": \n"+ doc.page_content for i,doc in enumerate(relevant_docs)])
+    print("Conversation:", conversation)
+          
+    # response = chain.run({"message":message, "conversation":conversation})
+    response = chain.predict(
+        message=message, 
+        conversation=conversation)
+
+    # print(chain.memory.buffer)
     return response
 
 def remove_punctuation_before_emojis(text):
@@ -122,11 +136,13 @@ def main():
     while True:
         message = input(colored(f"message: ", "blue"))
         response = chatbot(message)
-        processed_text = remove_punctuation_before_emojis(response)
-        split_msg = split_sentence(processed_text)
-        for s in split_msg:
-            time.sleep(0.2)
-            print(s)
-        maybe_print_meme()
+        print(response)
+        
+        # processed_text = remove_punctuation_before_emojis(response)
+        # split_msg = split_sentence(processed_text)
+        # for s in split_msg:
+        #     time.sleep(0.2)
+        #     print(s)
+        # maybe_print_meme()
 
 main()
